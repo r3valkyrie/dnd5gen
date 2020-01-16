afbeaced48f5d980c208ed232ec36510e692209a
r3valkyrie <valkyrie@r3valkyrie.com>
2020-01-16 16:35:59 -0500

    add deps to setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/r3valkyrie/dnd5gen",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'dice'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
