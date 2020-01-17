e36335fa819f090d568e614cd38ee051ef03e566
r3valkyrie <valkyrie@r3valkyrie.com>
2020-01-17 09:40:13 -0500

    add 'other' as human gender option

--- dnd5gen/char_races/human.py
+++ dnd5gen/char_races/human.py
@@ -1,25 +1,30 @@
-"""
-Returns a race object.
-"""
-from os import path
-from json import load
-from random import choice, randrange, sample
+try:
+    from os import path
+    from json import load
+    from random import choice, randrange, sample
+except ImportError:
+    raise ImportError
 
 
 class Human:
     def __init__(self, char_background):
         resource_path = path.join(path.dirname(__file__))
-        with open(resource_path + "/../resources/names_human.json") as human_names:
-            names = load(human_names)
-
         with open(resource_path + "/../resources/languages.json") as languages:
             langs = load(languages)
             langs.remove("Common")
 
         self.race = "Human"
-        self.gender = choice(['male', 'female'])
+        self.gender = choice(['male', 'female', 'other'])
         self.bonus_asi = [1, 1, 1, 1, 1, 1]
-        self.first_name = choice(names['first'][self.gender])
+
+        with open(resource_path + "/../resources/names_human.json") as human_names:
+            names = load(human_names)
+
+        if self.gender != 'other':
+            self.first_name = choice(names['first'][self.gender])
+
+        else:
+            self.first_name = choice(names['first'][choice(['male', 'female'])])
         self.last_name = choice(names['last'])
         self.full_name = f"{self.first_name} {self.last_name}"
         self.hair_color = choice(["Blonde", "Ginger", "Burgandy", "Honey",
